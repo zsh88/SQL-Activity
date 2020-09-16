6142e73bccc9bc010570a9e33a6e2bac58166bc2
Zeinab Sh <45148605+zsh88@users.noreply.github.com>
2020-09-16 12:02:15 +0430

    add 4 SQL file

---
+++ Activity04.sql
@@ -0,0 +1,16 @@
+/* Activity 4.01: Displaying Particular Columns from the Table */
+
+SELECT FirstName AS 'First Name', LastName AS 
+  'Last Name', Phone AS 'Phone Number'
+FROM Customers;
+
+/* Activity 4.02: Extracting the Top Five Highest Paid Items */
+
+SELECT 
+	Products.ProductName AS 'Product Name',
+    Products.NetRetailPrice AS 'Product Retail Price',
+    Products.AvailableQuantity AS 'Available Quantity',
+    Products.AvailableQuantity * Products.NetRetailPrice AS 'Total Price of Available QTY'
+FROM products
+ORDER BY Products.NetRetailPrice DESC
+LIMIT 5
